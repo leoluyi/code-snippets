2ee4982edd88846548047b83735c3ecf853ce07b
Leo Lu <leoluyi@users.noreply.github.com>
2018-04-11 14:35:00 +0800

    Create enc2utf8.sh

---
+++ shell/enc2utf8.sh
@@ -0,0 +1,28 @@
+#!/user/bin/bash
+
+if [ $# -lt 1]; then
+  echo "Usage "$0" <file_name>"
+  echo "Convert text files  to UTF-8"
+  exit
+fi
+
+
+for i in $*; do
+
+  if [ -f $i ]; then
+    encoding=$(chardet $i | cut -d ' ' -f 2)
+    if [ "$encoding" == "Big5" ]; then
+      encoding="cp950"
+    fi
+    
+    if [ "$encoding" != "utf-8"]; then
+      echo "encoding "$i" ("$encoding") to utf-8"
+      iconv -f "$encoding" -t utf-8 "$i" > "$i.tmp" &&
+      mv -f "$i.tmp" "$i"
+      rm -f "$i.tmp"
+    else
+      echo ""$i" ("$encoding") not modified"
+    fi
+  fi
+  
+done
