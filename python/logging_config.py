397828202b8e6255147c253650249cba5a439456
leoluyi <leo0650@gmail.com>
2019-07-13 14:21:13 +0800

    Update Logging

--- python/logging_config.py
+++ python/logging_config.py
@@ -1,26 +1,40 @@
-__ALL__ = ['log_console', 'LogFileHandler']
+__ALL__ = ['get_console_handler', 'LogFileHandler']
 
 import logging
 import logging.handlers
 import sys
 from pathlib import Path
-from datetime import time
-# import pytz
 
-log_formatter = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
-log_console = logging.StreamHandler(sys.stdout)
-log_console.setFormatter(log_formatter)
+LOG_FORMATTER = logging.Formatter('[%(asctime)s][%(levelname)s] %(message)s')
 
 
 class LogFileHandler(logging.handlers.TimedRotatingFileHandler):
     """docstring for LogFile"""
 
     def __init__(
-            self, log_file, when='midnight', interval=1, backupCount=30,
-            atTime=time(
-                0, 0, 0,
-                # tzinfo=pytz.timezone('ROC')
-            )):
+            self, log_file, when='midnight', interval=1,
+            backupCount=30, atTime=None):
+        """
+        Arguments:
+            log_file {[type]} -- File path for logging output.
+
+        Keyword Arguments:
+            when {str} -- Argument passed to logging.handlers.TimedRotatingFileHandler (default: {'midnight'})
+            interval {number} -- Argument passed to logging.handlers.TimedRotatingFileHandler (default: {1})
+            backupCount {number} -- Argument passed to logging.handlers.TimedRotatingFileHandler (default: {30})
+            atTime {[type]} -- Argument passed to logging.handlers.TimedRotatingFileHandler (default: {None})
+
+        Details:
+
+            One could use the folowing as `atTime` setting:
+
+            ```
+            from datetime import time
+            import pytz
+
+            time(0, 0, 0, tzinfo=pytz.timezone('ROC')))
+            ```
+        """
 
         # Ensure log folder existst
         log_file = Path(log_file).resolve()
@@ -32,4 +46,37 @@ class LogFileHandler(logging.handlers.TimedRotatingFileHandler):
             atTime=atTime
         )
 
-        self.setFormatter(log_formatter)
+        self.setFormatter(LOG_FORMATTER)
+
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setFormatter(LOG_FORMATTER)
+    return console_handler
+
+
+def get_logger_console_file(logger_name, log_file):
+    logger = logging.getLogger(logger_name)
+
+    logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
+
+    logger.addHandler(get_console_handler())
+    logger.addHandler(LogFileHandler(log_file))
+
+    # with this pattern, it's rarely necessary to propagate the error up to parent
+    logger.propagate = False
+
+    return logger
+
+
+if __name__ == '__main__':
+    LOG_FILE = 'log/my_app.log'
+
+    default_logger = logging.getLogger(__name__)
+    default_logger.setLevel(logging.DEBUG)  # better to have too much log than not enough
+
+    default_logger.addHandler(get_console_handler())
+    default_logger.addHandler(LogFileHandler(LOG_FILE))
+
+    # with this pattern, it's rarely necessary to propagate the error up to parent
+    default_logger.propagate = False
