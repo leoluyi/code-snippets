7ddc9b18469054acc08cac7dff8e4a6e5858cd67
Lu Yi <leo0650@gmail.com>
2021-11-07 13:28:25 +0800

    Add cli-fu

---
+++ cli-fu/fix-dropbox-finename-end-space.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Command Line Fu - sed + find tricks
+# https://www.youtube.com/watch?v=CyVV2FI7-DE
+
+command -v fd >/dev/null && \
+  fd -HI '\s+[.]\w+$' | sed -E 's/((.+?)\s+[.](\w+)?)$/"\1" "\2.\3"/' | \
+  xargs -n 2 mv
